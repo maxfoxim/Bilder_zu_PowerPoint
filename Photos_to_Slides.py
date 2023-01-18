baecab4e5e613b692f846866dac8aee758f29398
Stephan <47562121+maxfoxim@users.noreply.github.com>
2023-01-19 00:24:35 +0100

    Bilder mittig, Geoinfos

--- Photos_to_Slides.py
+++ Photos_to_Slides.py
@@ -8,27 +8,44 @@ Letzte Änderungen
 3.8.20: Mit Unterordner und PP-Name nach Ordner
 10.11.20 Anscheinend neue EXIF Formatierung.
 24.1.21 kleiner Bugfix
+17.1.23 Datum statt Kameradaten; Ortsinformationen
 """
+
+"""
+Hilfreiche Links:
+https://www.shibutan-bloomers.com/python-libraly-pptx-2_en/6310/
+"""
+
 
 # fuer pptx und exifread: pip install python-pptx und pip install exifread
 
 from pptx import Presentation
-from pptx.util import Inches
+from pptx.util import Inches,Cm
 import os
 from PIL import Image
 from PIL.ExifTags import TAGS
+from pptx.util import Pt
+
+from geopy.geocoders import Nominatim
+geoLoc = Nominatim(user_agent="GetLoc")
 
 
-
-Ordner="HighLow"
-Mit_UnterOrdner=True
-
+Ordner="Fotoabend2022"
+Mit_UnterOrdner=False
+Datum_Statt_Kamera_Daten=True
+Overview_Slide=False
+fontsize=12
 
 def belichtungszeit(zeit):
     if zeit<1:
         zeit=int(1/zeit)
         zeit="1/"+str(zeit)
     return zeit
+
+def gps_converter(North,East):
+    North_Dezi=North[0]+North[1]/60.+North[2]/3600.
+    East_Dezi = East[0]+ East[1]/60.+ East[2]/3600.
+    return North_Dezi,East_Dezi
 
 
 def get_exif(fn):
@@ -42,6 +59,16 @@ def get_exif(fn):
         decoded = TAGS.get(tag, tag)
         ret[decoded] = value
     return ret
+
+
+# convert pixels to inches
+def px_to_inches(path):    
+    im = Image.open(path)
+    width = im.width / 1#im.info['dpi'][0]
+    height = im.height / 1#im.info['dpi'][1]
+    print("Image Info",im.info,"breite",width,"höhe",height)
+
+    return (width, height)
 
 # Lösche alte Powerpointdatei, da nicht überschrieben wird
 try:
@@ -96,31 +123,65 @@ Bild_Nummer=1
 #for Datei in JPG_Dateien[:]:
 blank_slide_layout = prs.slide_layouts[6]
 slide = prs.slides.add_slide(blank_slide_layout)
+slide_size = (25.4, 14.29) # cm
+prs.slide_width, prs.slide_height = Cm(slide_size[0]), Cm(slide_size[1])
 
 
 
 for Datei in JPG_Dateien[:]:
+
     blank_slide_layout = prs.slide_layouts[6]
     print("Datei",Datei)
     slide = prs.slides.add_slide(blank_slide_layout)
-    left = top = right = bottom = Inches(0.5) # Platzierung der Bilder
+    left = top = right = bottom = Cm(0.5) # Platzierung der Bilder
     pfad_convert=Ordner+"/"+Datei[:-4]+"_Convert.JPEG"
     print("JPEG in Powerpoint",pfad_convert)
-    pic = slide.shapes.add_picture(pfad_convert, left, top, height= Inches(6.0)) #Maße der Bilder
+
+
+    #slide_size = (20, 11.25)
+    #slide_size = (16, 9) # Inches?
+
+    #prs.slide_width, prs.slide_height = Inches(slide_size[0]), Inches(slide_size[1])
+
+    img = px_to_inches(pfad_convert)
+    #left_calc = Cm(slide_size[0] - img[0]) / 2
+    #print("Left Berechnet",left_calc)
+    Bildhoehe=slide_size[1]-2
+    Bild_ratio=img[0]/img[1]*Bildhoehe
+    Folienmitte=slide_size[0]/2.  
+    left=Folienmitte-Bild_ratio/2.  
+    print("left",left,"Folienmitte",Folienmitte,"Bild_Ratio",Bild_ratio)
+    pic = slide.shapes.add_picture(pfad_convert, Cm(left), top, height= Cm(Bildhoehe)) #Maße der Bilder
+ 
     
+
     
     #Platzierung der EXIF-Daten
     left = top = width = height = Inches(1)
-    left = Inches(1.2)
-    top  = Inches(7.1)
-    width= Inches(2)
+    left = Cm(1.2)
+    top  = Cm(slide_size[1]-1.5) #0.1 oben
+    width= Cm(1)
     txBox = slide.shapes.add_textbox(left, top, width, height)
     tf = txBox.text_frame
-    tf.text = ""
+    exif_text = ""
+    p = tf.paragraphs[0]
+    run_kamera = p.add_run()
+
+    print(slide.shapes)
+    #slide.shapes.title.
+    #txBox.paragraphs[0].font.size = Pt(4)
+
     
+    top  = Cm(slide_size[1]-1.0) #0.1 oben
+    locationBox = slide.shapes.add_textbox(left, top, width, height)
+    tf_location = locationBox.text_frame
+    #locationtext.text = ""
+    q = tf_location.paragraphs[0]
+    run_location = q.add_run()
+
     if Datei[-3:]!="png":
-        #if True:
-        try:
+        if True:
+       # try:
             ausgabe=get_exif(Ordner+"/"+Datei)
             belichtungszeit_wert=belichtungszeit( ausgabe["ExposureTime"]  )
 
@@ -131,40 +192,79 @@ for Datei in JPG_Dateien[:]:
             print("Exposure Time",ausgabe["ExposureTime"],str(round(ausgabe["ExposureTime"],6)))
             print("Model",ausgabe["Model"])
             #print("Lens Model",ausgabe["LensModel"])
-            tf.text=tf.text+str(Bild_Nummer)+")   " 
-            tf.text=tf.text+"Brennweite: "+str(ausgabe["FocalLength"]) + "  ISO: "+str(ausgabe["ISOSpeedRatings"]) 
+            exif_text=exif_text+str(Bild_Nummer)+")   " 
+            exif_text=exif_text+"Brennweite: "+str(ausgabe["FocalLength"]) + "  ISO: "+str(ausgabe["ISOSpeedRatings"]) 
             try:
-                tf.text=tf.text+"  Blende: "+str(round( ausgabe["ApertureValue"]))
+                # muss hier noch verbessert werden
+                exif_text=exif_text+"  Blende: "+str(round( ausgabe["ApertureValue"]))
+                exif_text=exif_text+"  Blende: "+str(round( ausgabe["MaxApertureValue"]))
+
             except:
                 print("Problem mit Blende")
-                tf.text=tf.text+"  Blende: "+str(round( ausgabe["MaxApertureValue"]))
-            tf.text=tf.text+"  Belichtungszeit: "+str(belichtungszeit_wert)+" s"
+            exif_text=exif_text+"  Belichtungszeit: "+str(belichtungszeit_wert)+" s"
+            
+            if Datum_Statt_Kamera_Daten:
+                run_kamera.text=ausgabe["DateTime"]
+            else:
+                run_kamera.text=exif_text
 
+
+
+
+
+            # Geo Informationen einbauen    
+            try: 
+                North_Dezi,East_Dezi=gps_converter(ausgabe["GPSInfo"][2],ausgabe["GPSInfo"][4])
+                
+                # passing the coordinates
+                locname = geoLoc.reverse(f"{North_Dezi}, {East_Dezi}")
+
+                # printing the address/location name
+                print(locname.address)
+                run_location.text=locname.address
+            except:
+                print ("Keine GeoDaten")
             Bild_Nummer=Bild_Nummer+1
             print("-----------------------------------")
-        except:
+        #except:
             print("Fehler bei Exifdaten")
-            tf.text=Datei
+            #exif_text=Datei
             print("-----------------------------------")
+
+
+    font = run_kamera.font
+    font.name = 'Calibri'
+    font.size = Pt(fontsize)
+
+    font = run_location.font
+    font.name = 'Calibri'
+    font.size = Pt(fontsize)
+
 
 
 
 # Overview
-index=0
-for hoehe in range(4):
-    for laenge in range(5):
-        try:
-            left = top = right = bottom = Inches(0.5) # Platzierung der Bilder
-            left = Inches(0.5)+Inches(laenge*2)
-            top =  Inches(0.5)+Inches(hoehe*2)
-    
-            pfad_convert=Ordner+"/"+JPG_Dateien[index][:-4]+"_Convert.JPEG"
-            print("JPEG in Powerpoint Mehrfachplot",pfad_convert)
-            pic = slide.shapes.add_picture(pfad_convert, left, top, height= Inches(1.5)) #Maße der Bilder
-    
-            index=index+1
-        except:
-            print("Keine Bilder mehr für übersicht")
+if Overview_Slide:
+    # Empty Page for Overview
+    blank_slide_layout = prs.slide_layouts[6]
+    print("Datei",Datei)
+    slide = prs.slides.add_slide(blank_slide_layout)
+
+    index=0
+    for hoehe in range(4):
+        for laenge in range(5):
+            try:
+                left = top = right = bottom = Inches(0.5) # Platzierung der Bilder
+                left = Inches(0.5)+Inches(laenge*2)
+                top =  Inches(0.5)+Inches(hoehe*2)
+        
+                pfad_convert=Ordner+"/"+JPG_Dateien[index][:-4]+"_Convert.JPEG"
+                print("JPEG in Powerpoint Mehrfachplot",pfad_convert)
+                pic = slide.shapes.add_picture(pfad_convert, left, top, height= Inches(1.5)) #Maße der Bilder
+        
+                index=index+1
+            except:
+                print("Keine Bilder mehr für übersicht")
 
 
 
